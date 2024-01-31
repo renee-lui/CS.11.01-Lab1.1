86fe4756936704e2fe82fe0d1c7d58a219a4d730
renee-lui <reneerllui@gmail.com>
2024-01-31 09:13:42 +0800

    committed

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scan.nextInt();
+        int dateableAge = age/2 +7;
+        System.out.println("You should date someone "+ dateableAge);
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scan.nextInt();
+        int pagenumberbyAge = 100-age;
+        System.out.println("You should read at least "+ pagenumberbyAge + " pages of a book.");
 
     }
 }
